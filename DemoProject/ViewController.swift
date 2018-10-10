7d31713f0d8a21f238e72b4d21b873ce4c443c40
yukishilo <noemail2498fh24@gmail.com>
2018-10-10 15:29:07 +0200

    add xcode and travis files

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Eon on 10/10/2018.
+//  Copyright © 2018 FutureLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+
+}
+
